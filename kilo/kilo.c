57a5d87549ecd41fa922534fcc962a9ad552c4e6
Sparsh Jain <sparshjain265@gmail.com>
2019-04-02 03:15:31 +0530

    Add Page Up and Down

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -20,7 +20,9 @@ enum editorKey
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,
 	ARROW_UP,
-	ARROW_DOWN
+	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 // data
@@ -86,20 +88,40 @@ int editorReadKey()
 			return '\x1b';
 		if (read(STDIN_FILENO, &seq[1], 1) != 1)
 			return '\x1b';
+
 		if (seq[0] == '[')
 		{
-			switch (seq[1])
+			if (seq[1] >= '0' && seq[1] <= '9')
 			{
-			case 'A':
-				return ARROW_UP;
-			case 'B':
-				return ARROW_DOWN;
-			case 'C':
-				return ARROW_RIGHT;
-			case 'D':
-				return ARROW_LEFT;
+				if (read(STDIN_FILENO, &seq[2], 1) != 1)
+					return '\x1b';
+				if (seq[2] == '~')
+				{
+					switch (seq[1])
+					{
+					case '5':
+						return PAGE_UP;
+					case '6':
+						return PAGE_DOWN;
+					}
+				}
+			}
+			else
+			{
+				switch (seq[1])
+				{
+				case 'A':
+					return ARROW_UP;
+				case 'B':
+					return ARROW_DOWN;
+				case 'C':
+					return ARROW_RIGHT;
+				case 'D':
+					return ARROW_LEFT;
+				}
 			}
 		}
+
 		return '\x1b';
 	}
 	else
@@ -278,6 +300,15 @@ void editorProcessKeypress()
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
 		break;
+
+	case PAGE_UP:
+	case PAGE_DOWN:
+	{
+		int times = E.screenrows;
+		while (times--)
+			editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+	}
+	break;
 
 	case ARROW_UP:
 	case ARROW_DOWN:
