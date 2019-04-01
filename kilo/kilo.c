bdeb7cc8c0cda3f03baa3039b95c4f0199ae1c18
Sparsh Jain <sparshjain265@gmail.com>
2019-04-02 01:00:24 +0530

    Misc Flags

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -17,8 +17,9 @@ void enableRawMode()
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
-	raw.c_iflag &= ~(IXON | ICRNL);
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
