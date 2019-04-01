6390ed38c9f0140c01e9cc1107ac0145bc8f2c68
Sparsh Jain <sparshjain265@gmail.com>
2019-04-02 03:21:34 +0530

    Add delete key

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -21,6 +21,7 @@ enum editorKey
 	ARROW_RIGHT,
 	ARROW_UP,
 	ARROW_DOWN,
+	DEL_KEY,
 	HOME_KEY,
 	END_KEY,
 	PAGE_UP,
@@ -103,6 +104,8 @@ int editorReadKey()
 					{
 					case '1':
 						return HOME_KEY;
+					case '3':
+						return DEL_KEY;
 					case '4':
 						return END_KEY;
 					case '5':
