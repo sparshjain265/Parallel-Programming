2b902848a6ba1d7532aadfc70a40492b1d0c810e
Sparsh Jain <sparshjain265@gmail.com>
2019-04-02 01:25:49 +0530

    CTRL(q) to quit

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -1,19 +1,37 @@
+// includes
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+// defines
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+// data
+
 struct termios orig_termios;
+
+// terminal
+
+void die(const char *s)
+{
+	perror(s);
+	exit(1);
+}
 
 void disableRawMode()
 {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+		die("tcsetattr");
 }
 
 void enableRawMode()
 {
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
@@ -25,9 +43,11 @@ void enableRawMode()
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 	
 }
+
+// init
 
 int main()
 {
@@ -37,7 +57,7 @@ int main()
 	while(1)
 	{
 		char c = '\0';
-		read(STDIN_FILENO, &c, 1);
+		if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
 		if(iscntrl(c))
 		{
 			printf("%d\r\n", c);
@@ -46,7 +66,7 @@ int main()
 		{
 			printf("%d ('%c')\r\n", c, c);
 		}
-		if(c == 'q') break;		
+		if(c == CTRL_KEY('q')) break;		
 	}
 
 	return 0;
