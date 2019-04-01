84395dff65d329dfeed9058922a5fb635d018712
Sparsh Jain <sparshjain265@gmail.com>
2019-04-02 03:00:26 +0530

    Add Moving Cursor

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -19,6 +19,7 @@
 
 struct editorConfig
 {
+	int cx, cy;
 	int screenrows;
 	int screencols;
 	struct termios orig_termios;
@@ -188,7 +189,10 @@ void editorRefreshScreen()
 
 	editorDrawRows(&ab);
 
-	abAppend(&ab, "\x1b[H", 3);
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+	abAppend(&ab, buf, strlen(buf));
+
 	abAppend(&ab, "\x1b[?25h", 6);
 
 	write(STDOUT_FILENO, ab.b, ab.len);
@@ -196,6 +200,31 @@ void editorRefreshScreen()
 }
 
 // input
+
+void editorMoveCursor(char key)
+{
+	switch (key)
+	{
+	case 'a':
+		E.cx--;
+		break;
+
+	case 's':
+		E.cy++;
+		break;
+
+	case 'd':
+		E.cx++;
+		break;
+
+	case 'w':
+		E.cy--;
+		break;
+
+	default:
+		break;
+	}
+}
 
 void editorProcessKeypress()
 {
@@ -208,6 +237,13 @@ void editorProcessKeypress()
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
 		break;
+
+	case 'w':
+	case 'a':
+	case 's':
+	case 'd':
+		editorMoveCursor(c);
+		break;
 	}
 }
 
@@ -215,6 +251,9 @@ void editorProcessKeypress()
 
 void initEditor()
 {
+	E.cx = 0;
+	E.cy = 0;
+
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
 		die("getWindowSize");
 }
