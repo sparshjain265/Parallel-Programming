dfdbd3ecf436100449a96de58654c66800156717
Sparsh Jain <sparshjain265@gmail.com>
2019-04-02 00:56:17 +0530

    Disable output processing

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -18,6 +18,7 @@ void enableRawMode()
 
 	struct termios raw = orig_termios;
 	raw.c_iflag &= ~(IXON | ICRNL);
+	raw.c_oflag &= ~(OPOST);
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
